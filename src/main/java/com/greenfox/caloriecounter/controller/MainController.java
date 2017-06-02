c704ba72f320c843ef978f9e106c3ab7c2d1af2e
norbert barics <norbert.barics@gmail.com>
2017-06-02 14:10:43 +0200

    added add or edit endpoint

--- src/main/java/com/greenfox/caloriecounter/controller/MainController.java
+++ src/main/java/com/greenfox/caloriecounter/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.greenfox.caloriecounter.controller;
 
 import com.greenfox.caloriecounter.repository.MealRepository;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +16,17 @@ public class MainController {
 
   @RequestMapping("/")
   public String mainpage(Model model){
-    model.addAttribute(mealRepo.findAll());
+    model.addAttribute("mealRepo", mealRepo.findAll());
     return "index";
   }
+
+  @RequestMapping("addMeal")
+  public String addMeal(Model model){
+    ArrayList<String> mealtype = new ArrayList<>(Arrays.asList("Breakfast", "Elevenses", "Lunch", "Snack", "Dinner",
+        "Midnight Snack"));
+    model.addAttribute("mealtype", mealtype);
+    return "/addMeal";
+  }
+
+
 }
