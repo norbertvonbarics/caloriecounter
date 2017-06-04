20e4d65101bfc785ff7471f78deeba85b127ba4f
norbert barics <norbert.barics@gmail.com>
2017-06-04 14:39:49 +0200

    you can delete and edit meals now

--- src/main/java/com/greenfox/caloriecounter/controller/MainController.java
+++ src/main/java/com/greenfox/caloriecounter/controller/MainController.java
@@ -10,6 +10,7 @@ import javax.swing.text.html.HTMLDocument.Iterator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.thymeleaf.expression.Lists;
@@ -42,6 +43,7 @@ public class MainController {
         Arrays.asList("Breakfast", "Elevenses", "Lunch", "Snack", "Dinner",
             "Midnight Snack"));
     model.addAttribute("mealtype", mealtype);
+    model.addAttribute("mealRepo", mealRepo.findAll());
     return "addMeal";
   }
 
@@ -51,6 +53,38 @@ public class MainController {
       @RequestParam(name = "choosedType") String mealtype) {
     int cal = Integer.parseInt(newCalorie);
     mealRepo.save(new Meal(mealtype, newMeal, cal));
+    return "redirect:/";
+  }
+
+  @RequestMapping(value = "/deleteMeal")
+  public String deleteTodo(@RequestParam(name = "delete") long id) {
+    mealRepo.delete(id);
+    return "redirect:/";
+  }
+
+  @RequestMapping(value = "/{id}/edit")
+  public String edit(Model model, @PathVariable("id") Long id) {
+    ArrayList<String> mealtype = new ArrayList<>(
+        Arrays.asList("Breakfast", "Elevenses", "Lunch", "Snack", "Dinner",
+            "Midnight Snack"));
+    model.addAttribute("mealtype", mealtype);
+
+    model.addAttribute("meal", mealRepo.findOne(id));
+    return "editMeal";
+  }
+
+  @RequestMapping(value = "/{id}/edit/update")
+  public String updated(@PathVariable("id") Long id, @RequestParam("description") String description,
+      @RequestParam(value = "calories") int calories,
+      @RequestParam(value = "choosedType") String choosedType) {
+
+    Meal meal = mealRepo.findOne(id);
+    meal.setDescription(description);
+    meal.setCalories(calories);
+    meal.setType(choosedType);
+
+    mealRepo.save(meal);
+
     return "redirect:/";
   }
 }
---
+++ src/main/java/com/greenfox/caloriecounter/controller/RestController.java
@@ -0,0 +1,8 @@
+package com.greenfox.caloriecounter.controller;
+
+/**
+ * Created by viki on 2017. 06. 04..
+ */
+public class RestController {
+
+}
